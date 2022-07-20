836aa5b2e21dbe2513f7021ca63eb0ef4a018ab8
Tyler Tidman <tyler.tidman@draak.ca>
2022-07-20 13:39:42 -0400

    Add in original, official English Diceware wordlists

--- dw2wl.sh
+++ dw2wl.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# wget https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
-# wget https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt
-# wget https://www.eff.org/files/2016/09/08/eff_short_wordlist_2_0.txt
+files="${1}"
 
-for wordlist in *.txt; do
+if [ -z "${files}" ] ; then
+    echo "You need to specify a file or files to turn into a wordlist"
+    exit 1
+fi
+
+for wordlist in ${files}; do
     cat ${wordlist} | cut -f 2 > ${wordlist}.wordlist
     rename -f 's/_wordlist//g' *.wordlist
     rename -f 's/\.txt//g' *.wordlist
--- wl2dw.sh
+++ wl2dw.sh
@@ -4,9 +4,7 @@
 # seq 11111 66666 | grep -v 7 | grep -v 8 | grep -v 9 | grep -v 0  # 7776
 
 if [ $(wc -l ${1} | tr -s ' ' | cut -d ' ' -f 2) -le 1296 ]; then
-    echo smaller
     paste -d'\t' <(seq 1111 6666 | grep -v 7 | grep -v 8 | grep -v 9 | grep -v 0) ${1}
 else
-    echo larger
     paste -d'\t' <(seq 11111 66666 | grep -v 7 | grep -v 8 | grep -v 9 | grep -v 0) ${1}
 fi
